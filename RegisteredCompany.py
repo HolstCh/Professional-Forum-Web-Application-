01599245b1f2be3c1427744b1682b224d507b115
aashiki <aashik.ilangovan@ucalgary.ca>
2022-03-16 16:07:13 -0600

    Update RegisteredCompany.py

--- RegisteredCompany.py
+++ RegisteredCompany.py
@@ -9,6 +9,7 @@ class RegisteredCompany:
         self.email = email
         self.phoneNo = phoneNo
 
+#Method to conttact User from the registered Company using their employee ID and a message
     def contactUser(self, employeeID, message):
         self.employeeID = employeeID
         self.message = message
