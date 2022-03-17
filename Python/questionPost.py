769f4848b35a71411b0f88e18acdc52f7d51d173
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-17 00:01:42 -0600

    Update questionPost.py

--- Python/questionPost.py
+++ Python/questionPost.py
@@ -1,3 +1,16 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+from datetime import datetime
+
+app=Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'draGGun.!382'
+app.config['MYSQL_DB'] = 'seng401'
+
+mysql=MySQL(app)
+
 @app.route("/post/view/<id>", methods=["GET"])
 def viewPost(id):
     if request.method == "GET":
@@ -10,3 +23,6 @@ def viewPost(id):
         return render_template("viewPost.html", question=result1, answers=result2, qid=id)
     # end of if
 # end of def
+
+if __name__=='__main__':
+    app.run(debug=True)
