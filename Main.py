46711bf71afe2a5dad0f49b002253cacbb3b91b9
Syntax-Cipher <chad.holst@hotmail.com>
2022-03-19 11:33:45 -0600

    Login class/Sign up

--- Main.py
+++ Main.py
@@ -1,13 +1,58 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
 from App import app
 
+from Database import mysql
+from Filter import *
+from Login import *
+from Profile import *
+from ProfessionCategory import *
+from RegisteredCompany import *
+from UnregisteredCompany import *
+from Engineer import *
+
 HOST = "http://127.0.0.1:5000/"
 
 
 @app.route('/')
-def mainPage():  # put application's code here
+def home():  # put application's code here
+    return redirect(url_for('main'))
+
+
+@app.route('/main')
+def main():
     return render_template("mainPage.html")
 
 
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        myLogin = Login(username, password)
+        userExists = myLogin.authenticate()
+        if userExists:
+            return redirect(url_for("main"))
+        else:
+            return render_template('loginPage.html')
+    else:
+        return render_template('loginPage.html')
+
+
+@app.route('/signUp', methods=["POST", "GET"])
+def signUp():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        myLogin = Login(username, password)
+        userExists = myLogin.authenticate()
+        if userExists:
+            return render_template('signUp.html', data=username)
+        else:
+            myLogin.add_login()
+            return redirect(url_for("main"))
+    else:
+        return render_template('signUp.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
