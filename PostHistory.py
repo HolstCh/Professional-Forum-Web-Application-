3c165b6d046ff9b6d61fc10c886663d6943a5ef0
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-21 17:25:18 -0600

    Update PostHistory.py

--- PostHistory.py
+++ PostHistory.py
@@ -1,33 +1,28 @@
 from Database import mysql
 from App import app
 
-def questionHistory(username):
-    if request.method == "GET":
-        cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Question_Post WHERE Username=%s", (username,))
-        results=cursor.fetchall()
-
-        if results:
-            return render_template("questionHistory.html", username=username, data=results)
-        
-        else:
-            return render_template("questionHistory.html", username=username, msg="No question history...")
-
-def answerHistory(username):
-    if request.method == "GET":
-        cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Answer_Post WHERE Username=%s", (username,))
-        results1=cursor.fetchall()
-
-        if results1:
-            cursor.execute("""SELECT Question_Post.Title FROM Question_Post JOIN Answer_Post ON Question_Post.QuestionPostID
-                =Answer_Post.QID WHERE Answer_Post.Username=%s""", (username,))
+class PostHistory:
+    def questionHistory(username):
+            cursor=mysql.connection.cursor()
+            cursor.execute("SELECT * FROM Question_Post WHERE Username=%s", (username,))
             
-            results2=cursor.fetchall()
+            return cursor.fetchall()
+    # end of def
 
-            data=zip(results1, results2)
+    def answerHistory(username):
+            cursor=mysql.connection.cursor()
+            cursor.execute("SELECT * FROM Answer_Post WHERE Username=%s", (username,))
+            results1=cursor.fetchall()
 
-            return render_template("answerHistory.html", username=username, data=data)
+            if results1:
+                cursor.execute("""SELECT Question_Post.Title FROM Question_Post JOIN Answer_Post ON Question_Post.QuestionPostID
+                    =Answer_Post.QID WHERE Answer_Post.Username=%s""", (username,))
 
-        else:
-            return render_template("answerHistory.html", username=username, msg="No answer history...")
+                results2=cursor.fetchall()
+                data=zip(results1, results2)
+
+                return data
+
+            else:
+                return None
+    # end of def
