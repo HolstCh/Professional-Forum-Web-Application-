6c873975ac5bb8d046058478d13e75d0f6bcea90
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-21 17:17:04 -0600

    Adjusted to be used as a module

--- Search.py
+++ Search.py
@@ -1,33 +1,17 @@
-from Database import mysql
-from App import app
+class Search:
+    # Note: default constructor is used
 
-def search():
-    if request.method == "POST":
-        query=request.form.get("basicSearch")
+    def getQuery(self):
+        from app import request
+        
+        return request.form.get("basicSearch")
+    # end of def
 
-        cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Question_Post WHERE Body LIKE '%" + query + "%'")
-        results=cursor.fetchall()
+    def searchResults(self, query):
+        from Database import mysql
 
-        return redirect("./query=" + query)
-
-def searchResults(query, username):
-    if request.method == "GET":
         cursor=mysql.connection.cursor()
         cursor.execute("SELECT * FROM Question_Post WHERE Body LIKE '%" + query + "%' OR Title LIKE '%" + query + "%'")
-        results=cursor.fetchall()
 
-        if results:
-            return render_template("searchResults.html", data=results, username=username)
-
-        else:
-            return render_template("searchResults.html", msg="No results...")
-
-    elif request.method == "POST":
-        query=request.form.get("basicSearch")
-
-        cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Question_Post WHERE Body LIKE '%" + query + "%'")
-        results=cursor.fetchall()
-
-        return redirect("../query=" + query)
+        return cursor.fetchall()
+    # end of def
