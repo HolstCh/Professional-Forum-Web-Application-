91f21d69d3f26bf63a58f4d1e962163cbfb934ee
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-21 17:33:59 -0600

    Update PostHistory.py

--- PostHistory.py
+++ PostHistory.py
@@ -1,32 +1,33 @@
-from Database import mysql
-from App import app
-
 class PostHistory:
     def questionHistory(username):
-            cursor=mysql.connection.cursor()
-            cursor.execute("SELECT * FROM Question_Post WHERE Username=%s", (username,))
-            results=cursor.fetchall()
-            cursor.close()
-            
-            return results
+        from Database import mysql
+        
+        cursor=mysql.connection.cursor()
+        cursor.execute("SELECT * FROM Question_Post WHERE Username=%s", (username,))
+        results=cursor.fetchall()
+        cursor.close()
+
+        return results
     # end of def
 
     def answerHistory(username):
-            cursor=mysql.connection.cursor()
-            cursor.execute("SELECT * FROM Answer_Post WHERE Username=%s", (username,))
-            results1=cursor.fetchall()
+        from Database import mysql
+        
+        cursor=mysql.connection.cursor()
+        cursor.execute("SELECT * FROM Answer_Post WHERE Username=%s", (username,))
+        results1=cursor.fetchall()
 
-            if results1:
-                cursor.execute("""SELECT Question_Post.Title FROM Question_Post JOIN Answer_Post ON Question_Post.QuestionPostID
-                    =Answer_Post.QID WHERE Answer_Post.Username=%s""", (username,))
+        if results1:
+            cursor.execute("""SELECT Question_Post.Title FROM Question_Post JOIN Answer_Post ON Question_Post.QuestionPostID
+                =Answer_Post.QID WHERE Answer_Post.Username=%s""", (username,))
 
-                results2=cursor.fetchall()
-                cursor.close()
+            results2=cursor.fetchall()
+            cursor.close()
 
-                return zip(results1, results2)      # Bundle results from MySQL query
+            return zip(results1, results2)      # Bundle results from MySQL query
 
-            else:
-                cursor.close()
-                
-                return None     # If first query was empty, return none
+        else:
+            cursor.close()
+
+            return None     # If first query was empty, return none
     # end of def
