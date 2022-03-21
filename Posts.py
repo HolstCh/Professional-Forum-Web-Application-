ade9e9983760653cdb149056ed716a708b72384e
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-21 17:23:48 -0600

    Adjusted to be used as module

--- Posts.py
+++ Posts.py
@@ -2,40 +2,40 @@ from Database import mysql
 from App import app
 from datetime import datetime
 
-def viewPost(id):
-    if request.method == "GET":
+class Posts:
+    def viewPost(self, id):
         cursor=mysql.connection.cursor()
         cursor.execute("""SELECT Title, Username, Timestamp, Profession, ProfessionCategory, Body FROM 
             Question_Post WHERE QuestionPostID=""" + id)
+
         result1=cursor.fetchall()
         cursor.execute("SELECT Username, Timestamp, Body FROM Answer_Post WHERE QID=" + id)
         result2=cursor.fetchall()
+        
+        results=[result1, result2]
 
-        return render_template("viewPost.html", question=result1, answers=result2, qid=id)
-    # end of if
-# end of def
+        return results      # Return list of result from MySQL queries
+        # end of if
+    # end of def
 
-def postQuestion(username):
-    if request.method == "GET":
-        return render_template("test.html")
-    # end of if
-
-    elif request.method == "POST":
+    def postQuestion(self, username):
         title=request.form.get("title", None)
         body=request.form.get("ques", None)
         profession=request.form.get("prof", None)
-        global select
         timestamp=datetime.now().replace(microsecond=0)
 
+        global select   # Make variable accessible to if/else/elif blocks
+
+        # Check which dropdown menu option was selected and get all checkboxes under that category
         if profession == "Civil":
             select=request.form.getlist("civilCB", None)
-        
+
         elif profession == "Chemical":
             select=request.form.getlist("chemCB", None)
 
         elif profession == "Electrical":
             select=request.form.getlist("elecCB", None)
-        
+
         elif profession == "Geomatics":
             select=request.form.getlist("geoCB", None)
 
@@ -50,11 +50,8 @@ def postQuestion(username):
              VALUES (%s, %s, %s, %s, %s, %s)""", (username, title, body, timestamp, profession, select))
 
         mysql.connection.commit()
-        cursor.execute("SELECT MAX(QuestionPostID) FROM Question_Post")
+        cursor.execute("SELECT MAX(QuestionPostID) FROM Question_Post WHERE Username=%s", (username,))
         results=cursor.fetchone()
-
-        #print(results[0], file=sys.stdout)
-
-        return redirect("./view/" + str(results[0]))
-    # end of if
-# end of def
+        
+        return str(results[0])      # Return ID of most recent question posted by user
+    # end of def
