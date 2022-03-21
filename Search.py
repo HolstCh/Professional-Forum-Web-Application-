b55f267a428933359e4601b2aafbc9441e96a07a
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-21 17:33:02 -0600

    Update Search.py

--- Search.py
+++ Search.py
@@ -3,8 +3,9 @@ class Search:
 
     def getQuery(self):
         from app import request
+        query=request.form.get("basicSearch")
         
-        return request.form.get("basicSearch")
+        return query
     # end of def
 
     def searchResults(self, query):
@@ -12,6 +13,8 @@ class Search:
 
         cursor=mysql.connection.cursor()
         cursor.execute("SELECT * FROM Question_Post WHERE Body LIKE '%" + query + "%' OR Title LIKE '%" + query + "%'")
+        results=cursor.fetchall()
+        cursor.close()
 
-        return cursor.fetchall()
+        return results
     # end of def
