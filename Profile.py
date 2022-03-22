f9fa0dc17b42e9a5471ed9b2ef623bdba83fa7dd
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-21 22:43:32 -0600

    Updated functions

--- Profile.py
+++ Profile.py
@@ -24,32 +24,19 @@ class Profile:
     #end of def
 
     # Display profile in HTML
-    def display(self, username):
+    def getData(self, username):
         from Database import mysql
         
         cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Profiles")
+        cursor.execute("SELECT * FROM Profiles WHERE Username=%s", (username,))
         result=cursor.fetchall()
         cursor.close()
 
         return result
     # end of def
-
-    # Get data from MySQL table and allow user to edit it, where any edits will be sent to MySQL
-    def displayEditableProfile(self, username):
-        from App import request
-        from Database import mysql
-        
-            cursor=mysql.connection.cursor()
-            cursor.execute("SELECT * FROM Profiles")
-            result=cursor.fetchall()
-            cursor.close()
-
-            return result
-    # end of def
     
     def pushEdits(self, username):
-        from App import app, request
+        from App import request
         from Database import mysql
         
         fName=request.form.get("fName", None)
@@ -62,10 +49,12 @@ class Profile:
         desc=request.form.get("desc", None)
         pastProj=request.form.get("proj", None)
 
-        cursor.mysql.connection.cursor()
-        cursor.execute("""UPDATE Profiles SET firstName=%s, middleNames=%s, lastName=%s, email=%s, 
-            currentCompany=%s, profession=%s, skills=%s, description=%s, projects=%s""", (fName, mNames, lName, email, 
-            curComp, prof, skills, desc, pastProj))
+        cursor=mysql.connection.cursor()
+        cursor.execute("""UPDATE Profiles SET firstName=%s, middleNames=%s, lastName=%s, email=%s, currentCompany=%s,
+            profession=%s, skills=%s, description=%s, projects=%s WHERE Username=%s""", (fName, mNames, lName, email, 
+            curComp, prof, skills, desc, pastProj, username))
+        
+        mysql.connection.commit()
 
         cursor.close()
     # end of def
