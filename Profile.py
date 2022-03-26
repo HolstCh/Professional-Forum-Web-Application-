0111bc7f92ed08f335d40b48067c9ce32c8b2596
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-25 19:44:10 -0600

    Modified to work with PythonAnywhere

--- Profile.py
+++ Profile.py
@@ -3,7 +3,7 @@ class Profile:
     def createProfile(self, username):
         from App import request
         from Database import mysql
-        
+
         fName=request.form.get("fName", None)
         mNames=request.form.get("mNames", None)
         lName=request.form.get("lName", None)
@@ -13,10 +13,10 @@ class Profile:
         skills=request.form.get("skills", None)
         desc=request.form.get("desc", None)
         pastProj=request.form.get("proj", None)
-        
+
         cursor=mysql.connection.cursor()
-        cursor.execute("""INSERT INTO Profiles (Username, FirstName, MiddleNames, LastName, Email, CurrentCompany,
-            Profession, Skills, Description, Projects) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (username,
+        cursor.execute("""INSERT INTO Profiles (username, firstName, middleNames, lastName, email, currentCompany,
+            profession, skills, description, projects) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (username,
             fName, mNames, lName, email, curComp, prof, skills, desc, pastProj))
 
         mysql.connection.commit()
@@ -26,19 +26,19 @@ class Profile:
     # Display profile in HTML
     def getProfile(self, username):
         from Database import mysql
-         
+
         cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Profiles WHERE Username=%s", (username,))
+        cursor.execute("SELECT * FROM Profiles WHERE username=%s", (username,))
         result=cursor.fetchall()
         cursor.close()
 
         return result
     # end of def
-    
+
     def pushEdits(self, username):
         from App import request
         from Database import mysql
-        
+
         fName=request.form.get("fName", None)
         mNames=request.form.get("mNames", None)
         lName=request.form.get("lName", None)
@@ -50,18 +50,17 @@ class Profile:
         pastProj=request.form.get("proj", None)
 
         cursor=mysql.connection.cursor()
-        cursor.execute("""UPDATE Profiles SET FirstName=%s, MiddleNames=%s, LastName=%s, Email=%s, CurrentCompany=%s,
-            Profession=%s, Skills=%s, Description=%s, Projects=%s WHERE Username=%s""", (fName, mNames, lName, email, 
+        cursor.execute("""UPDATE Profiles SET firstName=%s, middleNames=%s, lastName=%s, email=%s, currentCompany=%s,
+            profession=%s, skills=%s, description=%s, projects=%s WHERE Username=%s""", (fName, mNames, lName, email,
             curComp, prof, skills, desc, pastProj, username))
-        
+
         mysql.connection.commit()
 
         cursor.close()
     # end of def
 
-    def addCompany(self, username):
+    def addCompany(self, username, name, position, start, end):
         from Database import mysql
-        from App import request
 
         name = request.form.get("company", None)
         position = request.form.get("position", None)
@@ -69,10 +68,11 @@ class Profile:
         end = request.form.get("end", None)
 
         cursor = mysql.connection.cursor()
-        cursor.execute("""INSERT INTO past_companies (Username, CompanyName, Start, End, Position) VALUES (%s, %s, %s, %s, %s)""",
+        cursor.execute("""INSERT INTO Past_Companies (Username, CompanyName, Start, End, Position) VALUES (%s, %s, %s, %s, %s)""",
             (username, name, start, end, position))
-        #cursor.execute(userQuery)
-        cursor.close()
+
+        mysql.connection.commit()
+    # end of def
 
     def getPastCompanies(self, username):
         from Database import mysql
@@ -83,3 +83,4 @@ class Profile:
         cursor.close()
 
         return result
+    # end of def
