10152d067347bc17f17d8f805f35aaa216c1aaa2
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-25 21:26:12 -0600

    Moved request.form.get to Main.py

--- Profile.py
+++ Profile.py
@@ -1,23 +1,13 @@
 class Profile:
     # Getting profile data from HTML and creating a new MySQL table in database to store data
-    def createProfile(self, username):
+    def createProfile(self, username, fName, mNames, lName, email, curComp, prof, skills, desc, pastProjs):
         from App import request
         from Database import mysql
-
-        fName=request.form.get("fName", None)
-        mNames=request.form.get("mNames", None)
-        lName=request.form.get("lName", None)
-        email=request.form.get("email", None)
-        curComp=request.form.get("curComp", None)
-        prof=request.form.get("prof", None)
-        skills=request.form.get("skills", None)
-        desc=request.form.get("desc", None)
-        pastProj=request.form.get("proj", None)
 
         cursor=mysql.connection.cursor()
         cursor.execute("""INSERT INTO Profiles (username, firstName, middleNames, lastName, email, currentCompany,
             profession, skills, description, projects) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (username,
-            fName, mNames, lName, email, curComp, prof, skills, desc, pastProj))
+            fName, mNames, lName, email, curComp, prof, skills, desc, pastProjs))
 
         mysql.connection.commit()
         cursor.close()
@@ -35,24 +25,14 @@ class Profile:
         return result
     # end of def
 
-    def pushEdits(self, username):
+    def pushEdits(self, username, fName, mNames, lName, email, curComp, prof, skills, desc, pastProjs):
         from App import request
         from Database import mysql
-
-        fName=request.form.get("fName", None)
-        mNames=request.form.get("mNames", None)
-        lName=request.form.get("lName", None)
-        email=request.form.get("email", None)
-        curComp=request.form.get("curComp", None)
-        prof=request.form.get("prof", None)
-        skills=request.form.get("skills", None)
-        desc=request.form.get("desc", None)
-        pastProj=request.form.get("proj", None)
 
         cursor=mysql.connection.cursor()
         cursor.execute("""UPDATE Profiles SET firstName=%s, middleNames=%s, lastName=%s, email=%s, currentCompany=%s,
             profession=%s, skills=%s, description=%s, projects=%s WHERE Username=%s""", (fName, mNames, lName, email,
-            curComp, prof, skills, desc, pastProj, username))
+            curComp, prof, skills, desc, pastProjs, username))
 
         mysql.connection.commit()
 
@@ -61,11 +41,6 @@ class Profile:
 
     def addCompany(self, username, name, position, start, end):
         from Database import mysql
-
-        name = request.form.get("company", None)
-        position = request.form.get("position", None)
-        start = request.form.get("start", None)
-        end = request.form.get("end", None)
 
         cursor = mysql.connection.cursor()
         cursor.execute("""INSERT INTO Past_Companies (Username, CompanyName, Start, End, Position) VALUES (%s, %s, %s, %s, %s)""",
