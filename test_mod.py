34e1914bded38f7a7f00dd307d00ddb3dc3208db
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-26 00:51:44 -0600

    Added routing tests

--- test_mod.py
+++ test_mod.py
@@ -1,3 +1,6 @@
+# cd "C:\Users\coolp\Documents\Dylan\School\Y3 - 2022 WINTER\SENG 401 - Software Architecture\Project\Forum_Project_SENG_401"
+# python -m pytest test_mod.py
+
 from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
 from App import app
 from Database import mysql
@@ -11,64 +14,50 @@ from Engineer import *
 from Search import *
 from Posts import *
 from PostHistory import *
-from Database import *
 import pytest
 import sqlite3
+import mysql.connector
+import unittest
 from unittest.mock import MagicMock
 
+#dbc=mysql.connector.connect(
+#    host="localhost",
+#    user="root",
+#    password="draGGun.!382",
+#    database="seng401"
+#)
 
-# fixture for running Flask
-@pytest.fixture()
-def appTest():
-    testMyApp = Flask(__name__)
-    testMyApp.config.update({
-        "TESTING": True,
-    })
-    # other setup can go here
-    yield testMyApp
-    # clean up / reset resources here
+#cursor=dbc.cursor()
 
+#@pytest.fixture()
+#def appTest():
+#    testMyApp = Flask(__name__)
+#    testMyApp.config.update({ "TESTING": True })
+#    yield testMyApp
 
-@pytest.fixture
-def client(appTest):
-    return appTest.test_client()
+#@pytest.fixture
+#def client(appTest):
+#    return appTest.test_client()
 
-
-@pytest.fixture
-def runner(appTest):
-    return appTest.test_cli_runner()
-
-
-# create fixture for database session
-@pytest.fixture
-def session():
-    connection = sqlite3.connect(':memory:')
-    db_session = connection.cursor()
-
-    yield db_session
-    db_session.close()
-
-
-@pytest.fixture
-def setupDB(session):
-    session.execute('''CREATE TABLE Users
-                          (Username varchar(255) NOT NULL, Password varchar(255), PRIMARY KEY (Username))''')  # 3
-    session.execute('INSERT INTO Users VALUES ("chad", "holst")')
-    session.connection.commit()
-
+#@pytest.fixture
+#def runner(appTest):
+#    return appTest.test_cli_runner()
 
 # Login class tests:
-@pytest.mark.usefixtures("setupDB")
-def test_validate(session):
-    myLogin = Login("chad", "holst", session)
-    expected = myLogin.validate()
-    actual = [("chad", "holst"), ]
-    print(expected, actual)
-    assert expected == actual, "input should be valid and produce list of tuples"
+#def test_validate():
+#    #from Login import Login
+
+#    myLogin = Login("chad", "holst")
+#    actual = myLogin.validate()
+#    expected = [("chad", "holst"), ]
+#    print(expected, actual)
+#    assert expected == actual, "input should be valid and produce list of tuples"
 
 
 # Filter class test:
 def test_professionType():
+    from Filter import Filter
+
     myFilter = Filter()
     professionFilter = "Engineer"
     myListOfTuples = [("I", "Should", "Be", "Removed", "Since", "Index", "7 is", "Not Engineer"),
@@ -76,3 +65,79 @@ def test_professionType():
     expected = [("I", "Should", "Be", "Stay", "Since", "Index", "7 is", "Engineer"), ]
     actual = myFilter.professionType(myListOfTuples, professionFilter)
     assert expected == actual, "tuple should be removed if profession column from QUESTION_POSTS is different than professionFilter"
+
+
+# Routing tests
+class routeTests(unittest.TestCase):
+    #global tester
+    tester=app.test_client()
+
+    def test_homePage(self):
+        response=self.tester.get("/")
+        
+        assert response.status_code == 302, "Page should redirect"
+
+    def test_mainPage(self):
+        response=self.tester.get("/unregisteredMain")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_registeredMain(self):
+        response=self.tester.get("/registeredMain/test1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_query(self):
+        response=self.tester.get("/query=a")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_login(self):
+        response=self.tester.get("/login")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_signUp(self):
+        response=self.tester.get("/createProfile/test1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_createProfile(self):
+        response=self.tester.get("/signUp")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_viewProfile(self):
+        response=self.tester.get("/profile/view/test1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_editProfile(self):
+        response=self.tester.get("/profile/edit/test1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_questionHistory(self):
+        response=self.tester.get("/questions/test1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_answerHistory(self):
+        response=self.tester.get("/answers/test1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_postQuestion(self):
+        response=self.tester.get("/post/question")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_viewPost(self):
+        response=self.tester.get("/post/view/1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
+
+    def test_addCompany(self):
+        response=self.tester.get("/addCompany/test1")
+
+        assert response.status_code == 200, "Page should exist and load without errors"
