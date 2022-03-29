140771fe6ddcbbba08291c244c501b746bfd5ff0
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2022-03-29 12:29:27 -0600

    Delete ProfessionCategory.py

--- ProfessionCategory.py
+++
@@ -1,9 +0,0 @@
-from Database import mysql
-from App import app
-#Author: Gibran Akmal
-
-#initialize class
-class ProfessionCategory:
-    #set engg type data member (String)
-    def __init__(self, engg):
-        self.engg
