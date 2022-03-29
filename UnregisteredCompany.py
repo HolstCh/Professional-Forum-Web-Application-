5671055caeca381caa70fe175153e12a91bfd128
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2022-03-29 12:29:50 -0600

    Delete UnregisteredCompany.py

--- UnregisteredCompany.py
+++
@@ -1,9 +0,0 @@
-from Database import mysql
-from App import app
-#Author: Aashik
-
-#initialize class
-class UnregisteredCompany:
-    #set engg type data member (String)
-    def __init__(self, engg):
-        self.engg = engg
