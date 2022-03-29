74f447d1bc3b5fa1c7cffe6350b3c518b69edfe7
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2022-03-29 13:46:14 -0600

    Take out developer database credentials

--- Database.py
+++ Database.py
@@ -5,11 +5,11 @@ from App import app
 class Database:
     def getMySQL(self):
         try:
-            # Fill out the following fields before running!!!
-            app.config["MYSQL_HOST"] = "localhost"
-            app.config["MYSQL_USER"] = "chad"
-            app.config["MYSQL_PASSWORD"] = "@@Kawa1000"
-            app.config["MYSQL_DB"] = "seng401"
+            # Fill out the following fields before running!!! Note: the deployed version of the web application has its own credentials for the database
+            app.config["MYSQL_HOST"] = "Your Host"
+            app.config["MYSQL_USER"] = "Your Username"
+            app.config["MYSQL_PASSWORD"] = "Your Password"
+            app.config["MYSQL_DB"] = "Your Database Name"
 
             mysql = MySQL(app)  # user this object for accessing database in multiple classes
             return mysql
