774e976cfb7848efc8c114d9022339d97b96a75c
Dylan <75186098+mcdylan38@users.noreply.github.com>
2022-03-29 19:55:42 -0600

    Edited to remove unnecessary code

--- Login.py
+++ Login.py
@@ -4,22 +4,18 @@
 
 from App import app
 
-#import mysql.connector
-debug = True
-
 class Login:
     #Ctor for Login
-    def __init__(self, username, password, connection):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.connection = connection
         
+
     # Check if username and password combination exists
     def validate(self):
         query = "SELECT * FROM Users WHERE Username='" + self.username + "' AND Password='" + self.password + "'"
-        cursor = self.connection
-        cursor.execute(query)
-        result = cursor.fetchall()
+        result=self.executeQuery(query)
+        
         return result   
         
 
@@ -34,27 +30,13 @@ class Login:
         result = cursor.fetchall()
         mysql.connection.commit()
         return result
-
-
-    #Cross checks if a username and password correspond with eachother
-    #return boolean result
-    def authenticate(self):
-        userQuery = "SELECT Password FROM Users WHERE Username = '" + self.username + "';"
-        result = self.executeQuery(userQuery)
-        convert = str(result)
-        convert = convert[3:len(convert) - 4] #remove useless characters from front and end
-        if(self.password == convert):
-            return 1 #password matches with the one in table
-        elif (self.user_exist() == False):
-            return 0 #username does not exist
-        else:
-            return -1 #password does not match
         
 
     #For debugging. Prints all users in the user Table
     def printUsers(self):
         userQuery = "SELECT Username, Password FROM Users;"
         table_result = self.executeQuery(userQuery)
+
         for x in table_result:
             print(x)
     
@@ -63,6 +45,7 @@ class Login:
     def user_exist(self):
         userQuery = "SELECT 1 FROM Users WHERE Username = '"+ self.username +"';"
         result = self.executeQuery(userQuery)
+
         if(len(result) == 0):
             return False
         else:
@@ -76,9 +59,12 @@ class Login:
             userQuery = "INSERT INTO Users VALUES ('" + self.username + "','" + self.password+ "');"
             self.executeQuery(userQuery)
             print("added new user") #for debugging
+
             return True
+
         else:
             print("this username already exists") #for debugging
+
             return False
         
 
@@ -86,41 +72,3 @@ class Login:
     def set_password(self,password):
         userQuery = "UPDATE Users SET Password = '"+password + "' WHERE Username = '"+self.username +"';"
         self.executeQuery(userQuery)
-
-    
-
-if(debug == True):
-    # db_connection = mysql.connection.cursor()
-    # connection = mysql.connector.connect(host = "localhost", database = 'newdb', user = "root",passwd = "M0nkey.G1rlisme") #make sure to change password to correct one
-
-    #---------------------------TEST 1---------------------------------------
-    print("TEST 1")
-    myLogin = Login('tester1', 'password1') #create Login object
-    myLogin.add_login()    #add object to database table
-
-    if(myLogin.authenticate() == -1): #authentication should work
-        print("The password does not match the username")
-    elif (myLogin.authenticate() == 0):
-        print("The username does not exist")
-    else:
-        print("the Login was successful")
-
-    #--------------------------TEST 2-----------------------------------------------
-    print("\nTEST 2")
-    myLogin2 = Login('chloe646', 'other!') #this Login is not added to the table, so it should fail the authentication
-    if(myLogin2.authenticate() == -1): 
-        print("The password does not match the username")
-    elif (myLogin2.authenticate() == 0):
-        print("The username does not exist") #should output this, since the username is not in the table
-    else:
-        print("the Login was successful")
-
-    #---------------------------TEST 3--------------------------------------------
-    print("\nTEST 3")
-    myLogin3 = Login('tester1', 'password1')  #this should not work, since tester1 already exists
-    myLogin3.add_login()
-
-    #---------------------------TEST 4---------------------------------------------
-    print("\nTEST 4")
-    myLogin4 = Login('tester3', 'password1')  #this should be allowed
-    myLogin4.add_login()
